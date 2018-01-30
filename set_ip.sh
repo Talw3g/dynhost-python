eb7c97ca679bfde9c12e47af40e44bff70cdfe93
Thibault <tmarion90@gmail.com>
2018-01-30 11:18:25 +0100

    Modified set_ip to update DNS zone. NEWIP now fetched from ipinfo.io/ip.

--- set_ip.sh
+++ set_ip.sh
@@ -6,7 +6,7 @@ CONSUMERKEY=""
 ZONE=""
 ID=""
 
-NEWIP=$1
+NEWIP=`curl ipinfo.io/ip`
 
 URL="https://eu.api.ovh.com/1.0/domain/zone/$ZONE/dynHost/record/$ID"
 ACTION="PUT"
@@ -24,7 +24,23 @@ curl \
 -H "X-Ovh-Timestamp:$TS" \
 -H "X-Ovh-Signature:$SIGNATURE" \
 -H "X-Ovh-Consumer:$CONSUMERKEY" \
--d subDomain="dyntest" \
 -d ip=$NEWIP \
 $URL
 
+URL="https://eu.api.ovh.com/1.0/domain/zone/$ZONE/refresh"
+ACTION="POST"
+
+TS=`date +%s`
+
+PREHASH=$APPSECRET"+"$CONSUMERKEY"+"$ACTION"+"$URL"++"$TS
+HASH=`echo -n $PREHASH| sha1sum| awk -F' ' '{print $1}'`
+SIGNATURE='$1$'$HASH
+
+curl \
+-v \
+-X $ACTION \
+-H "X-Ovh-Application:$APPKEY" \
+-H "X-Ovh-Timestamp:$TS" \
+-H "X-Ovh-Signature:$SIGNATURE" \
+-H "X-Ovh-Consumer:$CONSUMERKEY" \
+$URL
