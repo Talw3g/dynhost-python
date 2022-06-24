7c530d25e11ede75f4bf961b038af0d175c51b72
Thibault <talw3g@protonmail.com>
2022-06-24 20:04:23 +0200

    Added some checks and output.

--- set_ip.py
+++ set_ip.py
@@ -2,20 +2,34 @@
 
 import ovh, requests
 import config
+from time import localtime, strftime
+
+print(strftime("%d/%m/%Y %H:%M:%S -----> ", localtime()), end="")
 
 zone = config.zone
 subdomain = config.subdomain
 
 client = ovh.Client()
 
-IP = requests.get('http://ipinfo.io/ip').content.decode("ascii")
+new_ip = requests.get('http://ipinfo.io/ip').content.decode("ascii")
 
-ids = client.get("/domain/zone/{:s}/dynHost/record".format(zone))
+ID = client.get(
+    "/domain/zone/{:s}/dynHost/record".format(zone),
+    subDomain=subdomain,
+    )[0]
 
-for ID in ids:
-    dh = client.get("/domain/zone/{:s}/dynHost/record/{:d}".format(zone, ID))
-    if dh["subDomain"] == subdomain:
-        client.put("/domain/zone/{:s}/dynHost/record/{:d}".format(zone, ID), ip=IP)
+old_ip = client.get(
+    "/domain/zone/{:s}/dynHost/record/{:d}".format(zone, ID)
+    )['ip']
 
-client.post("/domain/zone/{:s}/refresh".format(zone))
+if old_ip != new_ip:
+    client.put(
+        "/domain/zone/{:s}/dynHost/record/{:d}".format(zone, ID),
+        ip=new_ip,
+    )
+    client.post("/domain/zone/{:s}/refresh".format(zone))
+    print("New IP set : ", new_ip)
+
+else:
+    print("No change")
 
